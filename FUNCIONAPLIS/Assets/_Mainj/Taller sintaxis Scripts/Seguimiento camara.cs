621c2ca8edb8c2a36a2c0f55d9f10d1d115ef827
Galaa4 <santiagoadarveramirez@gmail.com>
2026-08-25 11:12:47 -0500

    Taller de sintaxis Unity

---
+++ FUNCIONAPLIS/Assets/_Mainj/Taller sintaxis Scripts/Seguimiento camara.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+ 
+public class SeguimientoCamara : MonoBehaviour
+{
+    [SerializeField] private Transform _objetivo;
+ 
+    private void Update()
+    {
+        transform.position = new Vector3(_objetivo.position.x, transform.position.y, transform.position.z);
+    }
+}
