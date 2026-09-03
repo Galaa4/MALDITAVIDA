8ad331c6ca865cb65a57485a86d9692903468c67
Mapaa4 <marango322@soyudemedellin.edu.co>
2026-09-03 11:47:23 -0500

    funcionó :') (Vidavidavidamasvida)

--- FUNCIONAPLIS/Assets/_Mainj/Taller sintaxis Scripts/HEAL.cs
+++ FUNCIONAPLIS/Assets/_Mainj/Taller sintaxis Scripts/HEAL.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class HEAL : MonoBehaviour
 {
+    [SerializeField] private int _Healamount = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        { 
+            PlayerStats stats = collision.GetComponent<PlayerStats>(); 
+
+            if (stats != null)
+            {
+                stats.SumarVida(_Healamount);
+
+                Destroy(gameObject);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- FUNCIONAPLIS/Assets/_Mainj/Taller sintaxis Scripts/Player Stats.cs
+++ FUNCIONAPLIS/Assets/_Mainj/Taller sintaxis Scripts/Player Stats.cs
@@ -3,9 +3,22 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private int _puntosVida = 100;
+    [SerializeField] private int _vidaMaxima = 100;
     public void RestarVida(int daño)
     {
         _puntosVida = _puntosVida - daño;
         _puntosVida -= daño;
     }
+    public void SumarVida(int cantidad)
+    {
+        _puntosVida += cantidad;
+
+        if  (_puntosVida > _vidaMaxima)
+        {
+            _puntosVida = _vidaMaxima;
+        }
+
+        Debug.Log("Vida actual" +  _puntosVida);
+    }
+
 }
